faf107ed25d826ccad948d6be0133572c5657eae
omar112209 <fauzanomar675@gmail.com>
2026-06-11 21:07:48 +0700

    fix: bypass encryption for session cookie to resolve 419 on Railway

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -12,6 +12,7 @@ class EncryptCookies extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'laravel_session',
+        'maztechgarage_session',
     ];
 }
